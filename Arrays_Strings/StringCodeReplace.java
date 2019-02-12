6693e50e24f026b7b68c43cdbde41fd51d0d1acd
telemachusghost <derick.falk@gmail.com>
2019-02-11 21:51:46 -0800

    Changes to 1.4

--- Arrays_Strings/StringCodeReplace.java
+++ Arrays_Strings/StringCodeReplace.java
@@ -1,30 +1,64 @@
 // First attempt 
 // In place algorithm
-// No external data structures used at all
-// 
 
+import java.util.Stack;
 public class StringCodeReplace {
     
     public static void stringReplace(char[] str, int length) {
- 
+       int spaceCount = 0;
+       int space;
+       Stack<Integer> spaces = new Stack<Integer>(); // Keep track of where spaces are
+
        for (int i = 0; i < length; i++) {
            
-	   if (str[i] == ' ') {
-	         for (int f = length-1; f > i; f--) {
-		     str[f+2] = str[f];
-		 }
-                 str[i] = '%';
-		 str[i+1] = '2';
-		 str[i+2] = '0';
-	   }
+	         if (str[i] == ' ') {
+               spaceCount++;
+
+           }
+       }
+       for (int f = length-1; f >= 0; f--) {
+		             if (spaceCount == 0) {
+                     break;  
+                 }
+                 if (str[f] == ' ') {
+                     
+                     str[f+2*spaceCount-2] = '%';
+                     str[f+2*spaceCount-1] = '2';
+                     str[f+2*spaceCount] = '0';
+                     spaceCount--;
+                     continue;
+                 }  
+                 str[f+2*spaceCount] = str[f];
+		   }
+	 }
 
        
-       }
-    }
+
 
     public static void main(String[] args) {
-        char[] test = {'a', ' ', 'b', ' ', ' '};
-        stringReplace(test,3);
+        char[] test = {'M',
+                       'r',
+                       ' ',
+                       'J',
+                       'o',
+                       'h',
+                       'n',
+                       ' ',
+                       'S',
+                       'm',
+                       'i',
+                       't',
+                       'h',
+                       ' ',
+                       't',
+                       ' ',
+                       ' ',
+                       ' ',
+                       ' ',
+                       ' ',
+                       ' ',
+                       };
+        stringReplace(test,15);
 	System.out.println(test);
     }
 
